8002cfd3d1096575634f5aadd9f21f53a13672e0
Alvaro Acevedo <alvaro.acevedo.ing@gmail.com>
2024-09-18 13:02:54 -0300

    add 404 page

--- src/app.js
+++ src/app.js
@@ -70,6 +70,12 @@ app.use('/privado', auth('user'), (req, res) => {
     res.send('Esta es una vista privada');
 });
 
+app.use((req, res, next) => {
+    res.status(404).render('404', {
+        title: '404 - Página no encontrada'
+    })
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
